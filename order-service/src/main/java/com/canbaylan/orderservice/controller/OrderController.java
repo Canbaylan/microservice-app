65768c2f53e43f5a73f27d2d80cb17cd183770ea
canbaylan <canbaylan34@gmail.com>
2023-08-25 17:40:13 +0300

    order-service added

---
+++ order-service/src/main/java/com/canbaylan/orderservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.canbaylan.orderservice.controller;
+
+import com.canbaylan.orderservice.dto.OrderRequest;
+import com.canbaylan.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order placed Successfully";
+    }
+}
