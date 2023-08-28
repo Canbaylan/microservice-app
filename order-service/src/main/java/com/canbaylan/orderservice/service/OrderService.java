69e4d5639c82a5d7a58ecd5606cbe5b2ce6f3423
canbaylan <canbaylan34@gmail.com>
2023-08-28 19:41:00 +0300

    Sleuth/Zipkin implemented

--- order-service/src/main/java/com/canbaylan/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/canbaylan/orderservice/service/OrderService.java
@@ -7,6 +7,8 @@ import com.canbaylan.orderservice.model.Order;
 import com.canbaylan.orderservice.model.OrderLineItems;
 import com.canbaylan.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,7 @@ import java.util.stream.Collectors;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -37,21 +40,29 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .collect(Collectors.toList());
 
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",uriBuilder ->
-                        uriBuilder.queryParam("skuCode",skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",uriBuilder ->
+                            uriBuilder.queryParam("skuCode",skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
-                .allMatch(InventoryResponse::isInStock);
+            boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                    .allMatch(InventoryResponse::isInStock);
 
-        if(allProductsInStock){
-            orderRepository.save(order);
-        } else {
-            throw new IllegalArgumentException("Product is not in stock, please try again later.");
+            if(allProductsInStock){
+                orderRepository.save(order);
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later.");
+            }
+        }   finally {
+            inventoryServiceLookup.end();
         }
+
+
+
 
 
     }
